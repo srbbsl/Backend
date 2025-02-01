ba7519c497cfad1597c969a2249cf09a0a249fba
srbbsl <saurabbsl@gmail.com>
2025-02-01 23:13:33 +0545

    jan 31.2 finished

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,51 @@
 import { User } from "../models/User.js";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 
-
-export const userLogin = (req, res) => {
+export const userLogin = async (req, res) => {
     console.log(req.body);
-    console.log(req.query);
-    return res.status(200).json({
-        message: 'login page'
+    const { email, password } = req.body;
+  try {
+
+    const isExist = await User.findOne({ email: email });
+    const passCom = bcrypt.compareSync(password, isExist.password);
+    const token = jwt.sign({
+        id: isExist._id
+    }, 'secret');
+
+    if (!isExist) {
+      return res.status(401).json({
+        message: 'invalid credential'
+      });
+    };
+
+    
+    if (!passCom) return res.status(401).json({
+        message: 'invalid credential'
     });
+ 
+    return res.status(200).json({
+     message: 'user succesfully login',
+     data: {
+        token,
+        email: isExist.email,
+     }
+    });
+  } catch (err) {
+    return res.status(400).json({
+      message: `${err}`,
+        });
+    };
+    
 };
 
 export const userRegister = async (req, res) => {
     console.log(req.body);
     const {username, email, password} = req.body;
     try {
-        const isExist = await User.findOne({
-            email: email
-        });
+        const isExist = await User.findOne({email: email});
+        const hashPassword = bcrypt.hashSync(password, 10);
         
         if(isExist) {
             return res.status(409).json({
@@ -28,7 +57,7 @@ export const userRegister = async (req, res) => {
         await User.create({
             username,  // username: username,
             email,     // email: email,
-            password,  // password: password,                                   
+            password: hashPassword  // password: password,                                   
         });
         return res.status(200).json({
             status: "Success",
