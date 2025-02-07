8e08da8b74852cb59080cd263315ec58f008698f
srbbsl <saurabbsl@gmail.com>
2025-02-07 15:34:03 +0545

    feb 4.1 finished

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
 import { Product } from "../models/Product.js";
 import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
 
 
 export const getAllProduct = async (req, res) => {
@@ -8,6 +8,18 @@ export const getAllProduct = async (req, res) => {
     try {
         const products = await Product.find({});
         return res.status(200).json(products);
+    } catch (err) {
+        return res.status(400).json({
+            message: `${err}`,
+        });
+    };
+};
+
+export const getSingleProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findById(id);
+        return res.status(200).json(product);
     } catch (err) {
         return res.status(400).json({
             message: `${err}`,
@@ -46,10 +58,6 @@ export const createProduct = async (req, res) => {
 export const removeProduct = async (req, res) => {
     const { id } = req.params;
     try {
-        if (!mongoose.isValidObjectId(id)) return res.status(400).json({
-            message: 'please proviede valid id',
-        });
-
         const isExist = await Product.findById(id);
         if(!isExist) return res.status(404).json({
             message: 'product not found',
@@ -63,6 +71,58 @@ export const removeProduct = async (req, res) => {
             });
         });
 
+    } catch (err) {
+        return res.status(400).json({
+            message: `${err}`,
+        });
+    };
+};
+
+export const updateProduct = async (req, res) => {
+    const { title, description, price, category, brand } = req.body;
+    const { id } = req.params;
+
+    try {
+        const isExist = await Product.findById(id);
+        if (!isExist) 
+           return res.status(400).json({
+            message: 'product not found',
+        });
+
+        const file = req.files?.image;
+
+        const common = {
+            title: title || isExist.title,
+            description: description || isExist.description,
+            price: Number(price) || isExist.price,
+            category: category || isExist.category,
+            brand: brand || isExist.brand,
+        };
+
+        if (file) {
+            const fileName = `${uuidv4()}-${file.name}`;
+
+            //new file yeta move garxa
+            file.mv(`./uploads/${fileName}`, (err) => {
+            if (err) 
+                return res.status(400).json({
+                    message: `${err}`,
+                });
+                //purano image remove garxa
+                fs.unlink(`./uploads/${isExist.image}`, async (e) => {
+                    //update garxa
+                    await Product.findByIdAndUpdate(id, {...common, image: fileName}, {runValidators: true});
+                });
+            });
+
+        } else {
+            await Product.findByIdAndUpdate(id, {...common}, {runValidators: true});
+        };
+       
+            return res.status(200).json({
+                message: 'product updated successfully',
+            });
+        
     } catch (err) {
         return res.status(400).json({
             message: `${err}`,
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createProduct, getAllProduct, removeProduct  } from '../controllers/productController.js';
+import { createProduct, getAllProduct, getSingleProduct, removeProduct, updateProduct  } from '../controllers/productController.js';
 import { checkAdmin, checkUser } from '../middleware/checkAuth.js';
 import { checkFile } from '../middleware/checkFile.js';
 import Joi from 'joi';
 import validate from 'express-joi-validation';
+import { idValidate } from '../middleware/idValidate.js';
 
 const router = express.Router();
 
@@ -19,6 +20,8 @@ const productSchema = Joi.object({
 
 router.route('/').get(getAllProduct).post(checkUser, checkAdmin, validator.body(productSchema), checkFile, createProduct);
 
-router.route('/:id').get(getAllProduct).delete(checkUser, checkAdmin, removeProduct);
+router.route('/:id').get(idValidate, getSingleProduct)
+.patch(idValidate, checkUser, checkAdmin, updateProduct)
+.delete(idValidate, checkUser, checkAdmin, removeProduct);
 
 export default router;
