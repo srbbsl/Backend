091b25cbc3d0a2e45fe0ee06938d9c90c5af246d
srbbsl <saurabbsl@gmail.com>
2025-02-20 14:07:41 +0545

    changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 import { User } from "../model/User.js";
+import bcrypt from 'bcrypt';
 
 
 export const userLogin = async (req, res) => {
@@ -19,13 +20,19 @@ export const userLogin = async (req, res) => {
 export const userRegister = async (req, res) => {
     const { username, email, password } = req.body;
     try {
+        const isExist = await User.findOne({ email: email })
+        if (isExist) return res.status(409).json({
+            message: 'user already exist',
+        });
+
+        const hashPassword = bcrypt.hashSync(password, 10);
         await User.create({
             username,
             email,
-            password,
-        })
+            password: hashPassword,
+        });
         return res.status(201).json({
-                message: 'registered successfully',
+            message: 'registered successfully',
         });
     } catch (err) {
         return res.status(400).json({
