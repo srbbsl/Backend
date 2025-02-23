0fed8161cb0545eb6b44c6a9b13ed9d5208c01ef
srbbsl <saurabbsl@gmail.com>
2025-02-23 20:02:29 +0545

    changes

--- middleware/fileCheck.js
+++ middleware/fileCheck.js
@@ -24,3 +24,23 @@ export const fileCheck = (req, res, next) => {
     });
     
 };
+
+export const updateFileCheck = (req, res, next) => {
+    const file = req.files?.image;
+    if(!file) return next();
+    
+    if(!imageTypes.includes(file.mimetype)) return res.status(400).json({
+        message: 'provide valid image type',
+    });
+
+    const imagePath = `/${uuidv4()}-${file.name}`;    
+
+    file.mv(`./uploads${imagePath}`, (err) => {
+        if(err) return res.status(400).json({
+            message: `${err}`,    
+        });
+        req.imagePath = imagePath; //req object ma naya key ra value add gareko
+        return next();  
+    });
+
+}
