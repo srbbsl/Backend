b4f2d9f9c22b839bc0c045bd17c84fce42244bba
srbbsl <saurabbsl@gmail.com>
2025-02-23 19:05:41 +0545

    changes

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
 import { Product } from "../model/Product.js";
-
+import fs from 'fs';
 
 export const getAllProduct = (req, res) => {
     return res.status(200).json({
@@ -25,4 +25,29 @@ export const addProduct = async (req, res) => {
             message: `${err}`,
         });
     }    
+};
+
+export const removeProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const product = await Product.findById(id);
+        if(!product) return res.status(404).json({
+            message: 'product not found',
+        });
+
+        await Product.findByIdAndDelete(id);
+        fs.unlink(`./uploads${product.image}`, (err) => {
+            if(err) return res.status(400).json({
+                message: `${err}`,
+            });
+        });
+
+        return res.status(200).json({
+            message: 'product deleted successfully',
+        });
+    } catch (err) {
+        return res.status(400).json({
+            message: `${err}`,
+        });
+    }
 };
