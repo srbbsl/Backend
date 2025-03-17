1e268b35002430f8adbd8ea3784a304d18efbc06
srbbsl <saurabbsl@gmail.com>
2025-03-17 17:10:42 +0545

    mar 2.2 finished

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,3 +1,4 @@
+import { Order } from "../model/Order.js";
 
 
 export const getAllOrder = async (req, res) => {
@@ -13,5 +14,16 @@ export const getOrderByUser = async (req, res) => {
 };
 
 export const createOrder = async (req, res) => {
+    const { products, totalAmount } = req.body;
+    try {
+        await Order.create({
+            userId: req.userId,
+            products,
+            totalAmount,
+        });
 
+        return res.status(201).json({ message: 'order created successfully '});
+    } catch (err) {
+        return res.status(400).json({ message: `${err}`});
+    }
 };
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { createOrder, getAllOrder, getorderById, getOrderByUser } from '../controllers/orderController.js';
+import { authCheck } from '../middleware/authCheck.js';
 
 const router = express.Router();
 
-router.route('/').get(getAllOrder).post(createOrder);
-router.route('/users/:id').get(getOrderByUser);
-router.route('/:id').get(getorderById);
+router.route('/').get(getAllOrder).post(authCheck, createOrder);
+router.route('/users/:id').get(authCheck, getOrderByUser);
+router.route('/:id').get(authCheck, getorderById);
 
 
 export default router;
