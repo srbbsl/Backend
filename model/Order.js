8780dd9028acc3969941ddd5893058872c8911e9
srbbsl <saurabbsl@gmail.com>
2025-03-19 11:01:27 +0545

    changes

--- model/Order.js
+++ model/Order.js
@@ -5,7 +5,7 @@ const orderSchema = new mongoose.Schema({
    userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
-    required: true,
+    // required: true,
    },
    products: [
     {
