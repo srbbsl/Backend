929c96de0f302c47f02c44b877e0cd4406be163e
srbbsl <saurabbsl@gmail.com>
2025-03-19 18:09:09 +0545

    userID of orders in database is missing mar 3.2 11.00

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { userLogin, userRegister } from '../controllers/userController.js';
+import { getUser, updateUser, userLogin, userRegister } from '../controllers/userController.js';
 import { loginSchema, registerSchema, validate } from '../utils/validatior.js';
+import { authCheck } from '../middleware/authCheck.js';
 
 
 const router = express.Router();
@@ -8,5 +9,7 @@ const router = express.Router();
 router.route('/login').post(validate.body(loginSchema), userLogin);
 
 router.route('/register').post(validate.body(registerSchema), userRegister);
+
+router.route('/:id').get(authCheck, getUser).patch(authCheck, updateUser);
 
 export default router;
